6e1fac4949b755b93babf23681c159cd0c11ef4d
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-03-02 16:50:46 -0600

    follow along to the first node video

---
+++ index.js
@@ -0,0 +1,9 @@
+var path = require('path');
+console.log(path.join(__dirname, 'data.json'));
+
+// var greet = 'Hello World';
+
+// for(i=0; i<100;i++) {
+//     console.log(i);
+// }
+// console.log(greet);
